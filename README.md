80b0f5553b3042f890b9be7b2bac827e21d76e5d
Toshit Chawda <r58playz@gmail.com>
2024-02-05 16:32:30 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# b85
+A simple commandline for the `base85` crate.
+
+```
+echo "some ascii content" | b85 encode > encoded
+cat encoded | b85 decode
+```
---
+++ src/main.rs
@@ -0,0 +1,38 @@
+use base85::{decode, encode};
+use std::env::args;
+use std::io::{stdin, Read};
+
+fn main() {
+    match args()
+        .nth(1)
+        .expect("need either encode or decode")
+        .as_str()
+    {
+        "encode" => {
+            let mut input = Vec::new();
+            stdin()
+                .read_to_end(&mut input)
+                .expect("failed to read stdin");
+            print!("{}", encode(&input));
+        }
+        "decode" => {
+            let mut input = String::new();
+            stdin()
+                .read_to_string(&mut input)
+                .expect("failed to read stdin");
+            unsafe {
+                print!(
+                    "{}",
+                    std::str::from_utf8_unchecked(&decode(&input).expect("failed to decode"))
+                )
+            };
+        }
+        "-h" | "--help" | "help" => {
+            println!("usage: b85 encode|decode");
+            println!("       pipe the input into stdin.");
+        }
+        _ => {
+            println!("invalid args")
+        }
+    }
+}
